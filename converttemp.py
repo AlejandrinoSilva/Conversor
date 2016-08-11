9637f607cfda4702e4e0ca17d5dd38e585dd61f4
AlejandrinoSilva <ramon_al_sil@yahoo.com.ar>
2016-08-11 15:09:13 -0300

    Update converttemp.py

--- converttemp.py
+++ converttemp.py
@@ -1,4 +1,16 @@
 import os
-
 class Conversor():
-    
+    def Inicio(self):
+        os.system('clear')
+        print("==========================================\n=CONVERSOR DE TEMPERATURAS\n==========================================")
+        print("===MENU===================================")
+        print("> 1 Para convertir de Celsius => Farenheit")
+        print("> 2 Para convertir de Farenheit => Celsius")
+        print("> 0 Para Salir\n==========================================")
+        x = input("Elija una opción")
+        if x == "":
+            print("Es necesario que ingrese una opción para continuar")
+        
+   
+app = Conversor()
+app.Inicio()
